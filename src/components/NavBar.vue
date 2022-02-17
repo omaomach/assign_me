233b581d401b176b1a1f199f1017487f44c72947
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-02-17 11:46:09 +0300

    progress bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
 <div class="navbar">
     <div class="icon_buttons">
         <button class="btn" id="btn_home"><i class="fa fa-home" id="user_home"></i> Assign</button>
-        <button class="btn"><i class="fa fa-user" id="user_icon"></i></button>
+        <button class="btn" id="btn_user"><i class="fa fa-user" id="user_icon"></i></button>
         <button class="btn" id="btn_bell"><i class="fa fa-bell" id="user_bell"></i></button>
         <button class="btn" id="btn_angle"><i class="fa fa-angle-down" id="user_angle"></i></button>
         
@@ -47,12 +47,30 @@
 </div>
 
 <div class="footer">
+
+    <div class="container">
+    <table class="footer1_items">
+    <tr>
+    <td class="one">
+        <div id="progress">
+            <div id="bar">10%</div>
+        </div>
+    </td>
+    <td class="two">
     <div class="search_assignment">
         <form action="/action_page.php">
         <input type="text" placeholder="Search..." id="search_input">
         <button type="submit" id="btn_submit"><i class="fa fa-search" id="user_search"></i></button>
         </form>
-        
+    </div>
+    </td>
+    <td class="three">
+        j
+    </td>
+    </tr>
+
+    </table>
+
     </div>
 
 </div>
@@ -61,21 +79,46 @@
 </template>
 
 <script>
+
 export default {
     name: 'NavBar',
 
 }
+
+var i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("bar");
+        var width = 10;
+        var id = setInterval(frame, 10);
+        let frame = function frame() {
+            if (width >= 200) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%"
+                elem.innerHTML = width + "%"
+            }
+        }
+
+    }
+}
+move("bar");
+
 </script>
 
 <style>
 .navbar {
     background-color: #42b983;
-    padding: 15px;
+    padding: 8px;
     border-radius: 12px;
     color: #ffffff;
     position: fixed;
     margin-top: 10px;
-    width: 97%;
+    margin-left: 5px;
+    width: 99.25%;
     top: 0%;
     
     
@@ -102,19 +145,24 @@ export default {
 #user_home {
     color: #ffffff;
 }
-
+#btn_user {
+    padding: 10px 15.8px;
+}
 #btn_angle {
     border-radius: 50%;
+    padding: 10px 17.5px;
     float: right;
 }
 #btn_bell {
     border-radius: 50%;
+    padding: 10px 16px;
 }
 #btn_home {
     border-radius: 12px;
     color: #ffffff;
     float: left;
     margin-left: 0px;
+    padding: 10px 15.8px;
 }
 #add {
     border-radius: 12px;
@@ -162,11 +210,12 @@ export default {
 .icon_buttons {
     text-align: right;
     margin-top: 0px;
+    padding: 0;
 }
 
 .header {
   background-color: #ffffff;
-  margin-top: 100px;
+  margin-top: 98px;
   text-align: center;
 }
 #logoo {
@@ -227,10 +276,10 @@ export default {
 #column2 {
     float: right;
     width: 26.85%;
-    height: 420px;
+    height: 429.5px;
     background-color: #D3D3D3;
     margin-top: 40px;
-    margin-right: 8px;
+    margin-right: 41px;
     box-shadow: 10px 10px 5px grey;
 
 }
@@ -249,15 +298,15 @@ export default {
 
 h3 {
     margin: 40px 0 0;
-    color: #42b983;
+    color: grey;
 
 }
 .footer {
-    padding: 15px;
     background-color: #D3D3D3;
     border-radius: 12px;
-    margin-top: 55px;
-    width: 98%;
+    margin-top: 70px;
+    margin-left: 7.5px;
+    width: 99.10%;
 }
 #user_search {
     color: #ffffff;
@@ -265,8 +314,9 @@ h3 {
 #btn_submit {
     background-color: seagreen;
     border-radius: 50%;
-    padding: 6px 8px;
-    margin-left: 5px;
+    padding: 6px 10px;
+    margin-left: 8px;
+    margin-right: 148px;
 
 }
 #search_input {
@@ -288,6 +338,43 @@ td {
     border: 5px solid #D3D3D3;
     border-radius: 12px;
     
-}  
+}
+.container {
+    padding: 0;
+}
+.footer1_items {
+    padding: 1.5px;
+    width: 100%;
+}
+.one {
+    padding: 0;
+    text-align: left;
+}
+.two {
+    padding: 0;
+    text-align: center;
+    
+    
+} 
+.three {
+    padding: 0;
+    text-align: right;
+    
+}
+#progress {
+  width: 200%;
+  background-color: #ddd;
+  border-radius: 12px;
+  perspective: 1px;
+  overflow: hidden;
+}
 
+#bar {
+  width: 10%;
+  height: 30px;
+  background-color: #04AA6D;
+  text-align: center;
+  line-height: 30px;
+  color: white;
+}
 </style>
